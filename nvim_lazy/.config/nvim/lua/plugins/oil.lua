b4d382c36ea2fea624af79a52d5b379935395718
Griffin <doylegriffin32@gmail.com>
2024-10-02 22:09:33 +1000

    readd oil

---
+++ nvim_lazy/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "stevearc/oil.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {
+            default_file_explorer = true,
+            show_hidden = true,
+            is_hidden_file = function(name, bufnr)
+                return vim.startswith(name, '.')
+            end,
+            -- This function defines what will never be shown, even when `show_hidden` is set
+            is_always_hidden = function(name, bufnr)
+                return false
+            end,
+        },
+        keys = {
+            {"-", "<CMD>Oil<CR>", desc="Open parent directory"},
+        },
+    }
+}

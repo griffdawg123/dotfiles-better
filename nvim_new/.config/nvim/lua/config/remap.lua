ae0bf99a082b8380cf11c9d6ad85d269c58d4425
Griffin <doylegriffin32@gmail.com>
2026-05-18 18:15:38 +1000

    nice looking setup

--- nvim_new/.config/nvim/lua/config/remap.lua
+++ nvim_new/.config/nvim/lua/config/remap.lua
@@ -7,9 +7,4 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
-vim.keymap.set("n", "<c-k>", ':wincmd k<CR>')
-vim.keymap.set("n", "<c-j>", ':wincmd j<CR>')
-vim.keymap.set("n", "<c-h>", ':wincmd h<CR>')
-vim.keymap.set("n", "<c-l>", ':wincmd l<CR>')
-
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP Code Action" })
--- nvim_new/.config/nvim/lua/plugins/oil.lua
+++ nvim_new/.config/nvim/lua/plugins/oil.lua
@@ -1,24 +1,29 @@
 return {
-    {
-        "stevearc/oil.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {
-            default_file_explorer = true,
-            view_options = {
-                show_hidden = true,
-            },
-            is_hidden_file = function(name, bufnr)
-                return vim.startswith(name, '.')
-            end,
-            -- This function defines what will never be shown, even when `show_hidden` is set
-            is_always_hidden = function(name, bufnr)
-                return false
-            end,
-        },
-        keys = {
-            {"-", "<CMD>Oil<CR>", desc="Open parent directory"},
-        },
-    }
+	{
+		"stevearc/oil.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			default_file_explorer = true,
+			view_options = {
+				show_hidden = true,
+			},
+			is_hidden_file = function(name, bufnr)
+				return vim.startswith(name, ".")
+			end,
+			-- This function defines what will never be shown, even when `show_hidden` is set
+			is_always_hidden = function(name, bufnr)
+				return false
+			end,
+			keymaps = {
+				["<C-h>"] = false,
+				["<C-j>"] = false,
+				["<C-k>"] = false,
+				["<C-l>"] = false,
+			},
+		},
+		keys = {
+			{ "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+		},
+	},
 }
-
